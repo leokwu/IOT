1deb08ff99887725faef698b1fcdace3ca4faff9
leok_wu <leok.wu@rock-chips.com>
2022-02-27 16:17:22 +0800

    update notes

--- src/mqtt_async_control.c
+++ src/mqtt_async_control.c
@@ -1,18 +1,6 @@
-/*******************************************************************************
- * Copyright (c) 2012, 2020 IBM Corp.
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * and Eclipse Distribution License v1.0 which accompany this distribution. 
- *
- * The Eclipse Public License is available at 
- *   https://www.eclipse.org/legal/epl-2.0/
- * and the Eclipse Distribution License is available at 
- *   http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * Contributors:
- *    Ian Craggs - initial contribution
- *******************************************************************************/
+//
+// Created by leok on 2022/2/24.
+//
 
 #include <stdio.h>
 #include <stdlib.h>
